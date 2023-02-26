f3c980fbccea6e85ec9cf20d44c00aadc607b6fa
Mbicha <mbithicharlse@gamil.com>
2023-02-26 14:19:14 +0300

    Modified

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,8 +2,27 @@ const calculateNumber = './0-calcul';
 const assert = require('assert');
 
 describe ("calculateNumber", () => {
-    assert.equal(4, calculateNumber(1 ,3));
-    assert.equal(5, calculateNumber(1, 3.7));
-    assert.equal(5, calculateNumber(1.2, 3.7));
-    assert.equal(6, calculateNumber(1.5, 3.7));
+    it('floating point whole numbers', () => {
+        assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+    });
+    
+    it('rounding down b\'s floating point fractional number', () => {
+        assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+    });
+    
+    it('rounding down a and b\'s floating point fractional number', () => {
+        assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+    });
+    
+    it('rounding down a\'s floating point fractional number', () => {
+        assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+    });
 });
+// > calculateNumber(1, 3)
+// 4
+// > calculateNumber(1, 3.7)
+// 5
+// > calculateNumber(1.2, 3.7)
+// 5
+// > calculateNumber(1.5, 3.7)
+// 6
